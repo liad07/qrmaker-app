b1d5ecf0d67f76a126fe0c7d4d73dd08dc2a35d2
liadk07 <91607695+liad07@users.noreply.github.com>
2022-04-20 12:33:32 +0300

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace qrmaker
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            pictureBox1.ImageLocation = "https://api.qrserver.com/v1/create-qr-code/?data=" +textBox1.Text;
+        }
+    }
+}
